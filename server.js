a5e9e6fe65905c0b5c47e25b20eea33ece2d2bba
Soham Dave <dave.soham2000@gmail.com>
2021-04-23 20:31:29 +0530

    Backend ready

--- server.js
+++ server.js
@@ -1,20 +1,45 @@
 const express = require("express");
+const bodyparser = require('body-parser');
 const app = express();
 
-app.get("/sendSMS", function (req, res) {
-  var accountSid = "AC5506ff75bf701114c0d7bac3e2b3694d"; // Your Account SID from www.twilio.com/console
-  var authToken = "8a13914903a10eacc8e24ec68a9ba3f5"; // Your Auth Token from www.twilio.com/console
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(express.urlencoded());
+app.use(express.json());
 
-  var twilio = require("twilio");
-  var client = new twilio(accountSid, authToken);
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  client.messages
-    .create({
-      body: "Testing from API",
-      messagingServiceSid: 'MG64d2d8f2598c07c6325f6399d1e5b408',
-      to: "+919920211185", // Text this number
-    })
-    .then((message) => res.send(`The message with id: ${message} was sent!`));
+    next();
 });
 
-app.listen( process.env.PORT ||3000);
+
+
+const accountSid = 'AC5506ff75bf701114c0d7bac3e2b3694d'; 
+const authToken = '619cf9f6b015928f7fac3549589e945d'; 
+const client = require('twilio')(accountSid, authToken); 
+app.post("/sendSMS", (req, res, next) => {
+    
+    console.log(req.body.name);
+    // res.status(200).json({
+    //     "Name": req.body.name
+    // }); 
+    client.messages 
+    .create({ 
+        body: `Criminal detected. Name: ${req.body.name}.`,  
+        messagingServiceSid: 'MG64d2d8f2598c07c6325f6399d1e5b408',      
+        to: '+919920211185' 
+    }) 
+    .then(message => console.log(message.sid)) 
+    .done(() => {
+        res.status(200).json({
+            "Message": "Sent"
+        });
+    });
+});
+app.get("/", (req,res,next) => {
+    console.log("Inside");
+    next();
+})
+app.listen(process.env.PORT || 5000, () => console.log('Server listening'));
