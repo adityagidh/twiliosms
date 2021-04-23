dbe8eb86189aeacb7f2c80353d4710f5c322573c
Soham Dave <dave.soham2000@gmail.com>
2021-04-23 14:17:31 +0530

    Tokens added

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const express = require("express");
 const app = express();
 
 app.get("/sendSMS", function (req, res) {
-  var accountSid = "AC1b7ba37757a90a53808cb1f07d2792b5"; // Your Account SID from www.twilio.com/console
-  var authToken = process.env.TWILIO_AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
+  var accountSid = "AC5506ff75bf701114c0d7bac3e2b3694d"; // Your Account SID from www.twilio.com/console
+  var authToken = "8a13914903a10eacc8e24ec68a9ba3f5"; // Your Auth Token from www.twilio.com/console
 
   var twilio = require("twilio");
   var client = new twilio(accountSid, authToken);
 
   client.messages
     .create({
-      body: "Hello from Node",
-      to: "phone_number", // Text this number
-      from: "+14342265201", // From a valid Twilio number
+      body: "Testing from API",
+      messagingServiceSid: 'MG64d2d8f2598c07c6325f6399d1e5b408',
+      to: "+919920211185", // Text this number
     })
     .then((message) => res.send(`The message with id: ${message} was sent!`));
 });
